988a6ac62ec15946849fe3860742a19de78e7f80
Liv <lopez.olivia17@outlook.com>
2021-04-14 13:35:00 -0500

    adjusted index, employee, and manager

---
+++ _test_/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require('../lib/Employee')
+
+describe("Employee", () =>{
+
+})
+
+// functions are needed to perform test 
+// guessing constructor function alone 
--- index.js
+++ index.js
@@ -1,112 +1,108 @@
-const inquirer = require ('inquirer');
-const fs = require ('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 const questions = () => {
-  return inquirer.prompt ([
-    {
+  return inquirer.prompt([{
       type: 'input',
-      name: `team manager's name`,
+      name: `manager_name`,
       message: `What is the team manager's name?`
     },
     {
       type: 'input',
-      name: `team manager's employee id`,
+      name: `manager_employee_id`,
       message: `What is the team manager's employee id?`
     },
     {
       type: 'input',
-      name: `team manager's email address`,
+      name: `manager_email_address`,
       message: `What is the team manager's email address?`
     },
     {
       type: 'input',
-      name: `team manager's office number`,
+      name: `manager_office_number`,
       message: `What is the team manager's office number?`
-    }
-  ]).then(() => {
-    inquirer.prompt([
-      {
-        type: 'list',
-        name: 'type of team member',
-        message: 'What type of team member would you like to add?',
-        choices: [
-          'Engineer',
-          'Intern',
-          'None'
-        ]
-      },
-    ]).then(() => {
-      if (engineer){
-        inquirer.prompt([
-          {
-            type: 'input',
-            name: `engineer's name`,
-            message: `Engineer's name?`
-          },
-          {
-            type: 'input',
-            name: `engineer's employee id`,
-            message: `Engineer's employee id?`
-          },
-          {
-            type: 'input',
-            name: `engineer's email address`,
-            message: `Engineer's email address?`
-          },
-          {
-            type: 'input',
-            name: `engineer's gitHub username`,
-            message: `Engineer's Github?`
-          }
-        ]).then (() => {
-          generateHTML()
-        })
-      } else if (intern){
-        inquirer.prompt([
-          {
-            type: 'input',
-            name: `intern's name`,
-            message: `Intern's name?`
-          },
-          {
-            type: 'input',
-            name: `intern's employee id`,
-            message: `Intern's employee id?`
-          },
-          {
-            type: 'input',
-            name: `intern's email address`,
-            message: `intern's email address?`
-          },
-          {
-            type: 'input',
-            name: `intern's school`,
-            message: `intern's school?`
-          }
-        ]).then (() => {
-          generateHTML()
-        })
-      } else {
+    },
+    {
+      type: 'list',
+      name: 'type_of_employee',
+      message: 'What type of team member would you like to add?',
+      choices: [
+        'Engineer',
+        'Intern',
+        'None'
+      ]
+    },
+  ]).then((data) => {
+    if (data.type_of_employee === 'Engineer') {
+      inquirer.prompt([{
+          type: 'input',
+          name: `engineer_name`,
+          message: `Engineer's name?`
+        },
+        {
+          type: 'input',
+          name: `engineer_id`,
+          message: `Engineer's employee id?`
+        },
+        {
+          type: 'input',
+          name: `engineer_email_address`,
+          message: `Engineer's email address?`
+        },
+        {
+          type: 'input',
+          name: `engineer_gitHub_username`,
+          message: `Engineer's Github?`
+        }
+      ]).then(() => {
         generateHTML()
-      };
-    }).catch ((err) => console.error(err))
-  });
+      })
+    } else if (data.type_of_employee === 'Intern') {
+      inquirer.prompt([{
+          type: 'input',
+          name: `intern_name`,
+          message: `Intern's name?`
+        },
+        {
+          type: 'input',
+          name: `intern_id`,
+          message: `Intern's employee id?`
+        },
+        {
+          type: 'input',
+          name: `intern_email_address`,
+          message: `intern's email address?`
+        },
+        {
+          type: 'input',
+          name: `intern_school`,
+          message: `intern's school?`
+        }
+      ]).then(() => {
+        generateHTML()
+      })
+    } else {
+      generateHTML()
+    };
+  }).catch((err) => console.error(err))
 };
 
 // function to write html file 
-function writeToFile (fileName, data) {
+function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (error) => {
     console.log('file saved')
   });
 };
 
 // function to initialize app 
-function init(){
+function init() {
   questions()
-  .then ((data) => writeToFile('Team.html', generateHTML(data)))
-  .then (() => console.log('Successfully wrote Team.html!'))
-  .catch ((err) => console.error(err))
+    .then((data) => writeToFile('Team.html', generateHTML(data)))
+    .then(() => console.log('Successfully wrote Team.html!'))
+    .catch((err) => console.error(err))
 };
 
 // call initializing function
 init();
+
+module.exports = questions; 
